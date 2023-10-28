c264ba468a67909c48f142e1fd5edac2ee23380c
FabioPin <fajos.pinheiro@hotmail.com>
2023-10-28 15:19:10 +0100

    schema

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,33 @@ import './style.css'
 
 import { z } from "zod";
 
+//example of normal type delcaration for typescirpt
+// type City = {
+//   name: string
+// }
+
 const citySchema = z.object({
-  name: z.string(),
+  name: z.string(), // string
+  population: z.number(), // number
+  area: z.string().min(5), //min length
 });
 
-const lisbon = { name: "Lisbon" };
+type City = z.infer<typeof citySchema>;
 
-console.log(citySchema.parse(lisbon));
+const city = { name: "Castelo Branco", population: 177912, area: "1 438 km²" } as City;
+
+console.log(citySchema.parse(city));
+
+console.log(citySchema.safeParse(city)); //returns success
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Zod</h1>
-    <h3> City: ${lisbon.name}</h3>
+    <h1>Zod Learning</h1>
+    <h3> City: ${city.name}</h3>
+    <ul>
+      <li>Population: ${city.population}</li>
+      <li>Area: ${city.area}</li>
+    </ul>
   </div>
 `
 
