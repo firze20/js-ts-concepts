3e5bb836890a0b1e7adbf7727cd3cdbb4a56d228
FabioPin <fajos.pinheiro@hotmail.com>
2023-10-28 15:58:58 +0100

    passthrough and strict()

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,29 @@ import { z } from "zod";
 //   name: string
 // }
 
+enum Activities {
+  Football = "Football",
+  Library = "Library", 
+  Hiking = "Hiking",
+  Disco = "Disco",
+  Concerts = "Concerts"
+}
+
+// another way
+
+const activities = ["Football", "Library", "Hiking", "Camping", "Disco", "Concerts"] as const
+
 const citySchema = z.object({
   name: z.string(), // string
   population: z.number().gt(0), // number and greater than 0
   area: z.string().min(5), //min length
   rating: z.string().optional(), //optional stuff
-});
+  activities: z.enum(["Football", "Library", "Hiking", "Camping", "Disco", "Concerts"]).optional(), //enum
+  activitiesEnum: z.nativeEnum(Activities) // or z.nativeEnum(activities)
+}).passthrough(); //diferent keys can be assigned
+
+//strict() so to don't allow different assignment keys
+
 
 type City = z.infer<typeof citySchema>; // creating the type from z schema 
 
@@ -20,11 +37,17 @@ const city = {
   name: "Castelo Branco",
   population: 177912,
   area: "1 438 km²",
+  activitiesEnum: Activities.Concerts,
+  hasPools: true
 } as City;
 
 console.log(citySchema.parse(city));
 
 console.log(citySchema.safeParse(city)); //returns success
+
+console.log(citySchema.shape);
+
+console.log(citySchema.partial().parse(city))
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -33,6 +56,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
     <ul>
       <li>Population: ${city.population}</li>
       <li>Area: ${city.area}</li>
+      <li>Activities: ${city.activitiesEnum}</li>
     </ul>
   </div>
 `;
