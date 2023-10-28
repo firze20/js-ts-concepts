824692b37a99e1245ddb8ab438f2ef55ce029cb5
FabioPin <fajos.pinheiro@hotmail.com>
2023-10-28 15:05:17 +0100

    added zod schema

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
+import { z } from "zod";
+
+const citySchema = z.object({
+  name: z.string(),
+});
+
+const lisbon = { name: "Lisbon" };
+
+console.log(citySchema.parse(lisbon));
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    <h1>Zod</h1>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
